79169769b6e2c04efe81bd8d6879ae275cff909d
Aa Maulana <aamaulanabola10@gmail.com>
2024-12-06 20:01:14 +0700

    show data from movie db

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '5497119fe607a02621a85d7e0e376fcd',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
